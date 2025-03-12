ed967874e05e6794eb5bec6b1e55fb71e2797e50
Юрий Видманкин <vidmankin@mail.ru>
2025-03-12 21:02:11 +0500

    задание 1

---
+++ 2025.03.09/1.py
@@ -0,0 +1,22 @@
+#Ввод данных
+name = input("Введите Ваше Имя: ")
+suname = input("Введите Вашу Фамилию:")
+age = input("Введите свой год рождения:")
+var1=int(age)
+
+#запрос текущей даты
+from datetime import datetime
+
+nowyear = datetime.now().year
+
+var2=int(nowyear)
+
+year=var2-var1
+
+print(suname, name, f', {year}')
+
+
+#Введите Ваше Имя: Юрий
+#Введите Вашу Фамилию:Видманкин
+#Введите свой год рождения:1976
+#Видманкин Юрий , 49
