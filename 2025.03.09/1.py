e52b44981e02871381de2f8290d21a32c450d7f4
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:40 +0500

    2025.03.09: оформление

--- 2025.03.09/1.py
+++ 2025.03.09/1.py
@@ -1,22 +1,22 @@
-#Ввод данных
+from datetime import datetime
+
+
+# Ввод данных
 name = input("Введите Ваше Имя: ")
 suname = input("Введите Вашу Фамилию:")
 age = input("Введите свой год рождения:")
-var1=int(age)
+var1 = int(age)
 
-#запрос текущей даты
-from datetime import datetime
-
+# запрос текущей даты
 nowyear = datetime.now().year
-
-var2=int(nowyear)
-
-year=var2-var1
+var2 = int(nowyear)
+year = var2 - var1
 
 print(suname, name, f', {year}')
 
 
-#Введите Ваше Имя: Юрий
-#Введите Вашу Фамилию:Видманкин
-#Введите свой год рождения:1976
-#Видманкин Юрий , 49
+# Введите Ваше Имя: Юрий
+# Введите Вашу Фамилию:Видманкин
+# Введите свой год рождения:1976
+# Видманкин Юрий , 49
+
--- 2025.03.09/3.py
+++ 2025.03.09/3.py
@@ -1,7 +1,11 @@
-time = int (input ('Время в минутах:'))
-Hour = int (time) // 60
-min = int (time) % 60
-print (f' {time} мин - это {Hour} час. {min} минут')
+time = int(input('Время в минутах:'))
 
-#Время в минутах:156
-#156 мин - это 2 час. 36 минут
+Hour = int(time) // 60
+min = int(time) % 60
+
+print(f' {time} мин - это {Hour} час. {min} минут')
+
+
+# Время в минутах:156
+# 156 мин - это 2 час. 36 минут
+
