9b2d41f75e4ca5dcf07923e0741fed7986793d1c
Gennadiy <renardes@gmail.com>
2025-04-09 22:28:13 +0500

    2025.03.17: оформление

--- 2025.03.17/1.py
+++ 2025.03.17/1.py
@@ -9,27 +9,28 @@ num2 = Decimal(num2)
 num3 = Decimal(num3)
 
 
-if num1>0:
-    num_1=num1
-elif num1<=0:
-     num_1=0
-if num2>0:
-    num_2=num2
-elif num2<=0:
-     num_2=0
-if num3>0:
-    num_3=num3
-elif num3<=0:
-     num_3=0         
+if num1 > 0:
+    num_1 = num1
+elif num1 <= 0:
+    num_1 = 0
 
-sum=num_1+num_2+num_3
+if num2 > 0:
+    num_2 = num2
+elif num2 <= 0:
+    num_2 = 0
+
+if num3 > 0:
+    num_3 = num3
+elif num3 <= 0:
+    num_3 = 0
+
+sum = num_1 + num_2 + num_3
+
+print("сумма положительных чисел", sum)
 
 
-print ("сумма положительных чисел", sum)
+# число 1: 4
+# число 2: -22
+# число 3: 1.5
+# сумма положительных чисел 5.5
 
-
-#число 1: 4
-#число 2: -22
-#число 3: 1.5
-#сумма положительных чисел 5.5
-    
